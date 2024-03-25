1290cd45a53b8faa8421050e2f0c5d6f2707635f
yadnyesh <yadnyesh.juvekar@gmail.com>
2024-03-25 22:04:25 +0530

    Initial Commit

---
+++ src/main/java/io/yadnyesh/weather/app/WeatherAppApplication.java
@@ -0,0 +1,13 @@
+package io.yadnyesh.weather.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherAppApplication.class, args);
+	}
+
+}
